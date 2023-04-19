2eb90597eeafc57e285ee3d9d2b327d3e51f10c7
Francisco Rafael Ruiz <116575917+RuizFrancisco@users.noreply.github.com>
2023-04-19 10:52:08 -0300

    subí el 4

---
+++ exercise4.py
@@ -0,0 +1,69 @@
+"""Conversiones Básicas"""
+
+
+"""
+Convertir los numeros de string a enteros y luego sumarlos.
+"""
+
+numero_01 = "123"
+numero_02 = "456"
+numero_03 = "789"
+numero_04 = "132"
+
+# COMPLETAR - INICIO
+suma_de_numeros = int(numero_01) + int(numero_02) + int(numero_03) + int(numero_04)
+print(f"Suma de los Numeros es: {suma_de_numeros}")
+# COMPLETAR - FIN
+
+assert suma_de_numeros == 1500
+
+
+"""
+Convertir los numeros de enteros a string y luego concatenarlos.
+"""
+
+numero_01 = 123
+numero_02 = 456
+numero_03 = 789
+
+# COMPLETAR - INICIO
+suma_de_numeros_string = str(numero_01) + str(numero_02) + str(numero_03)
+print(f"La concatenacion de los numeros da: {suma_de_numeros_string}")
+# COMPLETAR - FIN
+
+assert suma_de_numeros_string == "123456789"
+
+
+"""
+Convertir los numeros de binario, octal y hexadecimal a enteros y luego
+multiplicarlos.
+"""
+
+numero_binario = "0b111010110101110111101000000"
+numero_octal = "0o1425"
+numero_hexadecimal = "0x6f540"
+
+# COMPLETAR - INICIO
+multiplicacion_de_numeros = int(numero_binario,2) * int(numero_octal,8) * int(numero_hexadecimal,16)
+print(f"La multiplicacion de los numeros da: {multiplicacion_de_numeros}")
+# COMPLETAR - FIN
+
+assert multiplicacion_de_numeros == 44397345600000000
+
+
+"""
+Convertir todo los numeros a enteros y luego restarlos secuencialmente (El uno
+menos el dos menos el tres menos el cuatro).
+"""
+
+numero_01 = "987"
+numero_02 = "0x6f54F"
+numero_03 = "0o1234"
+numero_04 = 654
+
+# COMPLETAR - INICIO
+resultado_resta = int(numero_01) - int(numero_02,16) - int(numero_03,8) - numero_04
+print(f"La resta de los numeros da: {resultado_resta}")
+# COMPLETAR - FIN
+
+assert resultado_resta == -456350
